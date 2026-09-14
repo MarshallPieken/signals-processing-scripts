cbbd394fd51b79965fa8f4bcf224cb10a608fe43
HumanBean <pieken@marshall.edu>
2026-09-14 08:08:23 -0400

    Clean up comment in iq-length.py
    Remove incomplete comment about script utility.

--- iq-length.py
+++ iq-length.py
@@ -1,7 +1,9 @@
 #!/bin/env python3
-
+"""
+This script simply converts the incoming int 32 file into a flat stream of complex numbers.
+"""
 import numpy as np
-infile = input("Enter the PATH of the file to cut: ")
+infile = input("Enter the PATH of the file: ")
 
 x = np.fromfile(infile, dtype=np.int32, count=2_000_000)
 iq = x[0::2] + 1j*x[1::2]
